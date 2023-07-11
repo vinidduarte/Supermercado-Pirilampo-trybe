38f80b93f76c9494304a4613900e1428a667d2a2
Vinicius Drumond Duarte <viniciusdduarte@outlook.com>
2023-07-11 14:02:45 -0300

    Finalizando os itens

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,27 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let vitaminsItems = [];
+  let vitaminsMessage = "";
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].vitamins) {
+      let vitaminsInformation = [];
+      let vitaminsArray = Object.keys(stockProducts[index].vitamins);
+
+      for (let indexVi = 0; indexVi < vitaminsArray.length; indexVi += 1) {
+        vitaminsInformation.push(`${vitaminsArray[indexVi]} - ${stockProducts[index].vitamins.vitaminsArray[indexVi]}`)
+      }
+      vitaminsItems.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        vitaminsInformation
+      })
+    }
+
+
+
+
+  } return vitaminsItems;
 };
 
 module.exports = { getProductsRichInVitamin };
